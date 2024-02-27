e00634638e083dc835540acaa8c2acfab4c174b6
rodwol <r.goniche@alustudent.com>
2024-02-27 13:41:08 -0800

    commit

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for char in range(97,123):
+    if chr(char) == 'e' or chr(char) == 'q':
+        continue
+    else:
+        print(chr(char), end=' ')
---
+++ python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+First lesson with ALU about python if and else statements and loops
