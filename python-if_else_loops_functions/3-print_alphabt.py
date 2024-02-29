958f50e8a44d6013b1fd79cc4506412be4da57d9
rodwol <r.goniche@alustudent.com>
2024-02-29 00:26:27 -0800

    commit

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for char in range(97,123):
+for char in range(97, 123):
     if chr(char) == 'e' or chr(char) == 'q':
         continue
     else:
-        print("{}".format(chr(char)), end=' ')
+        print("{}".format(chr(char)), end='')
