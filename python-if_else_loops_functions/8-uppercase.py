aa03381660c07119fac2e9881ba6cfb7b22ddd84
rodwol <r.goniche@alustudent.com>
2024-02-29 14:03:50 -0800

    commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,5 @@ def uppercase(str):
         if ord(letters) in range(97, 123):
             change=chr(ord(letters) - 32)
             print("{}".format(change), end='')
-        elif letters == ' ' or letters == '\n':
-            print("{}".format(letters), end='')
         else:
-            print("{}".format(letters), end='')
+            print("{}".format(letters, ' ', '\n'), end='')
