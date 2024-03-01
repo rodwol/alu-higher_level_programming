7a566bb37e4948994613ddc54f7cab6be8270682
rodwol <r.goniche@alustudent.com>
2024-03-01 00:30:00 -0800

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
 
-    count = len(sys.argv) - 1
+    count = len(argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, argv[i + 1]))
