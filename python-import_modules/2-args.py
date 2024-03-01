b8dbc7122a7ea1488b02736d3e702ffebf3ba028
rodwol <r.goniche@alustudent.com>
2024-02-29 23:47:46 -0800

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
-        print(argv[i])
+        print("{}: {}".format(len(argv) - 1), argv[i])
