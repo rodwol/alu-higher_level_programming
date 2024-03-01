b743a2be45605aef16edb5531112933a989de1cd
rodwol <r.goniche@alustudent.com>
2024-02-29 21:18:31 -0800

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
 from sys import argv
 for i in range(1, len(argv)):
     if len(argv) == 0:
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 from sys import argv
 total = 0
 for i in range(1, len(argv)):
