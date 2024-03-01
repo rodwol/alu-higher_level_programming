806239580f65db0fc57a0892fab9c485461b46f2
rodwol <r.goniche@alustudent.com>
2024-03-01 13:17:32 -0800

    commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    change = ""
     for letters in str:
         if ord(letters) in range(97, 123):
             change = chr(ord(letters) - 32)
         elif letters == ' ':
             change = letters
             print("{}".format(change), end='')
+    change = ""
