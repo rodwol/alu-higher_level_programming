b8243a43d27c6dc363c5a4ed336ee35d82893d1e
rodwol <r.goniche@alustudent.com>
2024-03-01 00:13:41 -0800

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,12 @@
 
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 0:
+    if len(argv) == 1:
         print("0 arguments.")
-    elif len(argv) == 1:
+    elif len(argv) == 2:
         print("1 argument:")
+        print(argv[1])
     else:
         print("{} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(len(argv) - 1), argv[i])
+        for i in range(1, len(argv)):
+            print(argv[i])
