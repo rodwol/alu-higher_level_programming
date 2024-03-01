758f705df7b19c649daa9eba1c25e4b61af8040f
rodwol <r.goniche@alustudent.com>
2024-03-01 11:53:37 -0800

    commit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_value = float('-inf')  # Initialize to negative infinity to handle negative values
+    if a_dictionary is None:
+        return None
+
+    max_value = float('-inf')
     best_key = None
     for key, value in a_dictionary.items():
         if value > max_value:
