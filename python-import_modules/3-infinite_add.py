41824b3884ca7cc6ea2029b1587d1208063cbc35
rodwol <r.goniche@alustudent.com>
2024-02-29 21:46:21 -0800

    commit

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     total = 0
     for i in range(1, len(argv)):
-        total =  total + int(i)
-        print(total)
+        total = total + int(i)
+    print(total)
