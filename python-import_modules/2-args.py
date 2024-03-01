5a6f8ad614e27cf6bd59b7d4f9f5e3eefef9efd5
rodwol <r.goniche@alustudent.com>
2024-02-29 21:24:37 -0800

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-from sys import argv
-for i in range(1, len(argv)):
-    if len(argv) == 0:
-        print("0 arguments.")
-    elif len(argv) == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments: ".format(len(argv) - 1))
-        print("{}:{}".format(i, argv[i]))
+    from sys import argv
+    for i in range(1, len(argv)):
+        if len(argv) == 0:
+            print("0 arguments.")
+        elif len(argv) == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments: ".format(len(argv) - 1))
+            print("{}:{}".format(i, argv[i]))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-from sys import argv
-total = 0
-for i in range(1, len(argv)):
-total =  total + int(i)
-print(total)
+    from sys import argv
+    total = 0
+    for i in range(1, len(argv)):
+        total =  total + int(i)
+        print(total)
