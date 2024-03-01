a8a3261e33f24552970988f03a95be4e3157312c
rodwol <r.goniche@alustudent.com>
2024-02-29 19:14:09 -0800

    commit

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+for i in range(1, len(argv)):
+    if len(argv) == 0:
+        print("0 arguments.")
+    elif len(argv) == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments: ".format(len(argv) - 1))
+        print("{}:{}".format(i, argv[i]))
