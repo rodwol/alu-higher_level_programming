e89be6d7ac8ef07ec6621c3b4c4f82a207c194eb
rodwol <r.goniche@alustudent.com>
2024-03-06 00:22:17 -0800

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #an empty class Square that defines a square
-"""This module provides functionality related to squares."""
+"""
+This module provides functionality related to squares.
+"""
+
+
 class Square:
-     """
-    This class is intentionally left empty and can be used as a placeholder
-    """
+""" This class is intentionally left empty and can be used as a placeholde """
     pass
