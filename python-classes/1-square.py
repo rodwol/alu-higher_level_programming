26109ec00291da38988cfac99d57539dc8243586
rodwol <r.goniche@alustudent.com>
2024-03-05 23:45:08 -0800

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#define a class
 class Square:
     def __init__(self, size):
         self.__size = None #private is achieved through the double underscore syntax
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+#define a class square
 class Square:
     def __init__(self, size=0):
+        #initialize objects of the class
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = size #private instance attribute
