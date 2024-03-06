3fe1b5510aebd9ccb20d4a16bce64fe636b19376
rodwol <r.goniche@alustudent.com>
2024-03-05 21:51:22 -0800

    commit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    try:
-        for item in my_list[:x]:
-            try:
-                if isinstance(item, int):
-                    print("{:d}".format(item), end=' ')
-                    count += 1
-            except ValueError:
-                pass
-    except TypeError:
-        print("An exception occurred: x is larger than the length of my_list")
-        raise
-    print()
-    return count
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
