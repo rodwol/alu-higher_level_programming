7e7f86f596fa6b6cb421b9ac16368107f94d8a56
rodwol <r.goniche@alustudent.com>
2024-03-05 21:53:11 -0800

    commit

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except (ZeroDivisionError, TypeError):
-        print(" ")
-        return None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        if result is not None:
-            print("Inside result:{}".format(result))
+        print("Inside result: {}".format(div))
+    return (div)
