c524991d71c6155972703a48ff7407ee39597bfd
rodwol <r.goniche@alustudent.com>
2024-03-06 21:26:01 -0800

    commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -25,5 +25,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size  # private instance attribute
+
     def area(self):
         return self.__size ** 2
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,13 +18,18 @@ class Square:
             size : The size of the square.
         """
         self.__size = size
+
+    @property
     def size(self):
         return self.__size
+
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
