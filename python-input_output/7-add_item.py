d3213260c222faa755bdf7ee5569e10be687c5b1
rodwol <r.goniche@alustudent.com>
2024-03-18 14:43:31 -0700

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from os import path
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-def add_items_to_list_and_save(arguments):
-    """
-    Adds all arguments to a Python list and saves them to a file.
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-    Args:
-        arguments (list): A list of arguments to be added to the Python list.
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
 
-    Returns:
-        list: The updated list containing the arguments.
-    """
-    try:
-        existing_items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        existing_items = []
-
-    existing_items.extend(arguments)
-
-    save_to_json_file(existing_items, "add_item.json")
-
-    return existing_items
-
-if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    updated_list = add_items_to_list_and_save(arguments)
-    print("Updated list:", updated_list)
+save_to_json_file(obj_json_file, 'add_item.json')
