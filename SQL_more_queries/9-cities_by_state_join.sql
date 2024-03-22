f63f7ce06786f87ac247b7f1c76463533956389c
rodwol <r.goniche@alustudent.com>
2024-03-22 07:32:24 -0700

    commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
---
+++ SQL_more_queries/hbtn_0d_tvshows.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE hbtn_0d_tvshows;
