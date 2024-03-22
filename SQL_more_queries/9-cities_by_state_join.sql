7f2108317f6759445f753147d99f643843783fa1
rodwol <r.goniche@alustudent.com>
2024-03-22 07:40:58 -0700

    commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
