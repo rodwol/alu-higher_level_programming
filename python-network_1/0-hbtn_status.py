3dfbfb824b6700595aa0e540a1df39d4672e1014
rodwol <r.goniche@alustudent.com>
2024-03-23 21:56:26 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 url = 'https://alu-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     html = response.read()
-print("- Body response:")
+print("Body response:")
 print("\t- type:", type(html))
 print("\t- content:", html)
 print("\t- utf8 content:", html.decode('utf-8'))
--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 # takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
-"""Sends a request to the specified URL and displays the value of the X-Request-Id variable found in the header of the response.
+"""
+Sends a request to the specified URL and displays the value of the 
+X-Request-Id variable found in the header of the response.
 """
 
+import sys
 import urllib.request
-import sys 
 
-with urllib.request.urlopen(urllib.request.Request(sys.argv[1])) as response:
-    x_request_id = response.headers.get("X-Request-Id")
-print(x_request_id)
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        x_request_id = response.headers.get("X-Request-Id")
+    print(x_request_id)
