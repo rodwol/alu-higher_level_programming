704ecab11de14dcaa2d2c335402fbeda8bdba694
rodwol <r.goniche@alustudent.com>
2024-03-23 20:59:06 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # fetches https://alu-intranet.hbtn.io/status
 import urllib.request
-url = "https://alu-intranet.hbtn.io/status"
+
+url = 'https://alu-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
-    print("-Body response.")
-    print("\t-type:", type(body))
-    print("\t-content:", body)
+    html = response.read()
+print("- Body response:")
+print("\t- type:", type(html))
+print("\t- content:", html)
+print("\t- utf8 content:", html.decode('utf-8'))
--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
+"""Sends a request to the specified URL and displays the value of the X-Request-Id variable found in the header of the response.
+"""
 
 import urllib.request
 import sys 
