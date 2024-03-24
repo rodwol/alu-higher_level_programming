686b43d76939f2891743b7935bd91996cd4ab846
rodwol <r.goniche@alustudent.com>
2024-03-24 15:05:09 +0000

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server 
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; 
-SHOW GRANTS FOR 'user_0d_2'@'localhost'; 
+-- list all privlage for user user_0d_1 user_0d_2
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
