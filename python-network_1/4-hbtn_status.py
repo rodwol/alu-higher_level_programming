a4d004974fe436e0e029cb3c2cb89a004a1bc35a
rodwol <r.goniche@alustudent.com>
2024-03-24 13:12:35 +0000

    commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,9 +4,13 @@ fetches https://alu-intranet.hbtn.io/status
 """
 import requests
 
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+if response.status_code == 200:
+    data = response.json()
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print("Failed to fetch data. Status code:", response.status_code)
