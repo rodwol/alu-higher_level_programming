b61c4e11b2ea1d6dde470835f7e3d0f0315acba8
rodwol <r.goniche@alustudent.com>
2024-03-24 14:11:27 +0000

    commit

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,6 @@
---  lists all shows contained in the database hbtn_0d_tvshows
+-- lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
