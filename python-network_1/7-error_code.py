90a6133fb77ef4600a0114c7941791cc648e5e1b
rodwol <r.goniche@alustudent.com>
2024-03-24 13:02:39 +0000

    commit

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
 Script: url_response.py
-This script takes a URL as input, sends a request to that URL using the requests package, 
-and displays the body of the response. If the HTTP status code is greater than or equal 
-to 400, it prints an error message with the status code. It utilizes only the requests 
-and sys packages.
+This script takes a URL as input
+sends a request to URL using the requests package
+and displays the body of the response. If the HTTP status code is >= 400
+it prints an error message with the status code.
 
 """
 
@@ -18,3 +18,5 @@ if __name__ == "__main__":
 
     if response.status_code >= 400:
         print("Error code:", response.status_code)
+    else:
+        print(response.text)
