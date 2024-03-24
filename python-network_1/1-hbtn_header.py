37e753e410a7baa6474ff2dcadb1b472297e0105
rodwol <r.goniche@alustudent.com>
2024-03-24 15:29:25 +0000

    commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 # fetches https://alu-intranet.hbtn.io/status
+"""
+Fetches the status of a given URL and prints the X-Request-Id header.
+Usage: python3 script_name.py <URL>
+"""
+
 import urllib
 import sys
 
 
 if __name__ == '__main__':
+    # Fetch the URL and retrieve the X-Request-Id header
     with urllib.request.urlopen(sys.argv[1]) as response:
         x_request_id = response.getheader('X-Request-Id')
-
+    # Print the X-Request-Id header
     print(x_request_id)
