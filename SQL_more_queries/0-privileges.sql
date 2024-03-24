08bf3f11e4e17ca5e516ea9c409423d177a026f2
rodwol <r.goniche@alustudent.com>
2024-03-24 17:55:12 +0000

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,28 +1,7 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
-SELECT 
-    User, 
-    Host, 
-    Select_priv, 
-    Insert_priv, 
-    Update_priv, 
-    Delete_priv, 
-    Create_priv, 
-    Drop_priv, 
-    Grant_priv, 
-    References_priv, 
-    Index_priv, 
-    Alter_priv, 
-    Create_tmp_table_priv, 
-    Lock_tables_priv, 
-    Create_view_priv, 
-    Show_view_priv, 
-    Create_routine_priv, 
-    Alter_routine_priv, 
-    Execute_priv, 
-    Event_priv, 
-    Trigger_priv 
-FROM 
-    mysql.user 
-WHERE 
-    User IN ('user_0d_1', 'user_0d_2') 
-    AND Host = 'localhost';
+
+-- Show grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
