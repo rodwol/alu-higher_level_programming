a65a319ace392ac813f00ac85535957d20385740
rodwol <r.goniche@alustudent.com>
2024-03-24 21:11:37 +0000

    commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,13 +2,21 @@
 """
 Python script that fetches https://alu-intranet.hbtn.io/status
 """
+
 import requests
 
-if name == 'main':
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
+if __name__ == '__main__':
+    # URL to fetch
+    url = 'https://alu-intranet.hbtn.io/status'
+    
+    # Check if the URL starts with 'https://', if not, prepend it
+    if not url.startswith('https://'):
+        url = "https://" + url
+    
+    # Send GET request to the URL
     res = requests.get(url)
+    
+    # Print information about the response
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(res.text)))  # Type of the response content
+    print("\t- content: {}".format(res.text))    # Content of the response
