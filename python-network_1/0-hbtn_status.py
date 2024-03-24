487e94b863ce40bb5aa145b4a4464132e25985f8
rodwol <r.goniche@alustudent.com>
2024-03-24 21:21:09 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ import urllib.request
 # Define the URL to fetch
 url = 'https://intranet.hbtn.io/status'
 
-# If the URL doesn't start with 'https://', prepend it.
+# If the URL doesn't start with 'https://', prepend it
 if url.startswith('https://'):
     url = 'https://alu-intranet.hbtn.io/status'
 
@@ -19,9 +19,12 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as res:
         # Read the content of the response
         content = res.read()
-        
+
         # Print information about the response
         print("Body response:")
-        print("\t- type: {}".format(type(content)))  # Print the type of content
-        print("\t- content: {}".format(content))     # Print the content as bytes
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))  # Print the content as UTF-8 decoded string
+        print("\t- type: {}".format(type(content)))
+        # Print the type of content
+        print("\t- content: {}".format(content))
+        # Print the content as bytes
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        # Print the content as UTF-8 decoded string
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,15 +8,17 @@ import requests
 if __name__ == '__main__':
     # URL to fetch
     url = 'https://alu-intranet.hbtn.io/status'
-    
+
     # Check if the URL starts with 'https://', if not, prepend it
     if not url.startswith('https://'):
         url = "https://" + url
-    
+
     # Send GET request to the URL
-    res = requests.get(url)
-    
+    response = requests.get(url)
+
     # Print information about the response
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))  # Type of the response content
-    print("\t- content: {}".format(res.text))    # Content of the response
+    print("\t- type:", (type(response.text)))
+    # Type of the response content
+    print("\t- content:", (response.text))
+    # Content of the response
