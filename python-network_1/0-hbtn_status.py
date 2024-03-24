de139a2fc757bb70757628f83842f8ae47cb16be
rodwol <r.goniche@alustudent.com>
2024-03-24 21:08:09 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-# fetches https://alu-intranet.hbtn.io/status
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', (type(html))
-        print('\t- content:', (html))
-        print('\t- utf8 content:'(html.decode("utf-8")))
+if name == 'main':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
