2ae6c6485b26b636ee8b7b9ae8ac65c395edb7ad
rodwol <r.goniche@alustudent.com>
2024-04-11 09:32:58 +0000

    commit

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,26 +4,33 @@ Module: Calculator
 Provides functions for dividing elements in matrix
 """
 
-
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix.
 
     Parameters:
-        matrix: the matrix
-        div: div
+        matrix (list of lists): The matrix to be divided.
+            Each inner list represents a row of the matrix.
+            Elements of the matrix must be integers or floats.
+        div (int or float): The divisor by which to divide all elements of the matrix.
 
     Returns:
-        matrix: a new matrix divided by div
-    """
-if not all(isinstance(row, list) for row in matrix) or \
-   not all(isinstance(num, (int, float)) for row in matrix for num in row):
-    raise TypeError("matrix must be a matrix \
-(list of lists) of integers/floats")
+        list of lists: A new matrix where each element is the result of dividing
+            the corresponding element of the input matrix by div.
+            The result is rounded to 2 decimal places.
 
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix \
-(list of lists) of integers/floats")
+    Raises:
+        TypeError: If the input matrix is not a list of lists, or if any element
+            of the matrix is not an integer or float.
+        TypeError: If each row of the matrix doesn't have the same size.
+        TypeError: If the divisor is not a number.
+        ZeroDivisionError: If the divisor is zero.
+    """
+
+    # Check if matrix is a list of lists and contains only integers or floats
+    if not all(isinstance(row, list) for row in matrix) or \
+       not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     # Check if each row of the matrix has the same size
     row_sizes = [len(row) for row in matrix]
