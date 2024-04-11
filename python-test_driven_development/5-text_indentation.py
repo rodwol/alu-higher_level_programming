88ae0b3f34ef8ab4451e5d01e28c9dfe62ada7f1
rodwol <r.goniche@alustudent.com>
2024-04-11 09:26:07 +0000

    commit

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-"""
-Module: Printing
 
-"""
+
+'''
+   5-text_indentation
+   Contains a function that adds new line after characters
+'''
 
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after each of these characters: ., ? and :
-
-    Parameters:
-        text (str): The input text
-
-    Returns:
-        None
-    """
+    '''Python function to add two new lines to string at character points'''
     if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    trigger_chars = ['.', '?', ':']
-
-    # Iterate through the text
-    for i, char in enumerate(text):
-        print(char, end='')
-
-        # Check if the character triggers a new line
-        if char in trigger_chars:
-            print("\n")
-            # Skip the next two lines if they are newline characters or spaces
-            next_index = i + 1
-            while next_index < len(text) and (text[next_index] == '\n' or text[next_index] == ' '):
-                print(text[next_index], end='')
-                next_index += 1
-            print("\n")
-
-        # If the character is a newline, ensure two newlines are added
-        elif char == '\n':
-            print("\n", end='')
-            if i + 1 < len(text) and text[i + 1] == '\n':
-                print("\n", end='')
+        raise TypeError('text must be a string')
+    period = text.replace('.', '.\n\n')
+    period = period.replace('?', '?\n\n')
+    period = period.replace(':', ':\n\n')
+    period1 = period.split('\n')
+    for line in range(len(period1)):
+        print("{}".format(period1[line].strip()),
+              end=("" if (line == (len(period1) - 1)) else '\n'))
