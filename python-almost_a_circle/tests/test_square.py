6769ac866a59f0306deae776cb4b1be0299397ab
rodwol <r.goniche@alustudent.com>
2024-04-13 20:36:50 +0000

    commit

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,107 +1,113 @@
 #!/usr/bin/python3
 """
-Test for the Square class
+A module that test differents behaviors
+of the Square class
 """
-
 import unittest
-import json
-from models import square
+import pep8
 from models.base import Base
-Square = square.Square
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-    """Test the functionality of the Square class"""
-    @classmethod
-    def setUpClass(cls):
-        """set up the tests"""
-        Base._Base__nb_objects = 0
-        cls.s1 = Square(1)
-        cls.s2 = Square(2, 3)
+    """
+    A class to test the Square Class
+    """
+    def test_pep8_base(self):
+        """
+        Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/square.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
+        )
 
-    def test_id(self):
-        """Test for functioning ID"""
-        self.assertEqual(self.s1.id, 1)
-        self.assertEqual(self.s2.id, 2)
+    def test_getter(self):
+        r1 = Square(5)
+        self.assertEqual(r1.size, 5)
 
-    def test_size(self):
-        """Test for functioning size"""
-        self.assertEqual(self.s1.size, 1)
-        self.assertEqual(self.s2.size, 2)
+    def test_setter(self):
+        r1 = Square(5)
+        r1.size = 8
+        self.assertEqual(r1.size, 8)
+
+    def test_string(self):
+        r1 = Square(3)
+
+        with self.assertRaises(TypeError):
+            r1.size = "Hi"
+
+    def test_negative(self):
+        r1 = Square(6)
+
+        with self.assertRaises(ValueError):
+            r1.size = -5
+
+    def test_zero(self):
+        r1 = Square(6)
+
+        with self.assertRaises(ValueError):
+            r1.size = 0
+
+    def test_decimal(self):
+        r1 = Square(6)
+
+        with self.assertRaises(TypeError):
+            r1.size = 1.5
+
+    def test_tuple(self):
+        r1 = Square(7)
+
+        with self.assertRaises(TypeError):
+            r1.size = (2, 8)
+
+    def test_empty(self):
+        r1 = Square(7)
+
+        with self.assertRaises(TypeError):
+            r1.size = ''
+
+    def test_none(self):
+        r1 = Square(5)
+
+        with self.assertRaises(TypeError):
+            r1.size = None
+
+    def test_list(self):
+        r1 = Square(4)
+
+        with self.assertRaises(TypeError):
+            r1.size = [4, 7]
+
+    def test_dict(self):
+        r1 = Square(5)
+
+        with self.assertRaises(TypeError):
+            r1.size = {"hi": 5, "world": 8}
 
     def test_width(self):
-        self.assertEqual(self.s1.width, 1)
-        self.assertEqual(self.s2.width, 2)
+        r1 = Square(5)
+        r1.size = 6
+        self.assertEqual(r1.width, 6)
+        self.assertEqual(r1.height, 6)
 
-    def test_height(self):
-        """Test for functioning height"""
-        self.assertEqual(self.s1.height, 1)
-        self.assertEqual(self.s2.height, 2)
+    def test_to_dictionary(self):
+        Base._Base__nb_objects = 0
 
-    def test_x(self):
-        """Test for functioning x"""
-        self.assertEqual(self.s1.x, 0)
-        self.assertEqual(self.s2.x, 3)
+        s1 = Square(10, 2, 1, 9)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 9, 'x': 2, 'size': 10, 'y': 1}
+        self.assertEqual(s1_dictionary, expected)
 
-    def test_y(self):
-        """Test for functioning y"""
-        self.assertEqual(self.s1.y, 0)
-        self.assertEqual(self.s2.y, 0)
+        s1 = Square(1, 0, 0, 9)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 9, 'x': 0, 'size': 1, 'y': 0}
+        self.assertEqual(s1_dictionary, expected)
 
-    def mandatory_size(self):
-        """Test that width is a mandatory arg"""
-        with self.assertRaises(TypeError):
-            s = Square()
-
-    def size_typeerror(self):
-        """Test non-ints for size"""
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            s = Square("hello")
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            s = Square(True)
-
-    def test_x_typeerror(self):
-        """Test non-ints for x"""
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            s = Square(1, "hello")
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            s = Square(1, True)
-
-    def test_y_typeerror(self):
-        """Test non-ints for y"""
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            s = Square(1, 1, "hello")
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            s = Square(1, 1, True)
-
-    def test_size_valueerror(self):
-        """Test ints <= 0 for size"""
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s = Square(-1)
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s = Square(0)
-
-    def test_x_valueerror(self):
-        """Test ints < 0 for x"""
-        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
-            s = Square(1, -1)
-
-    def test_y_valueerror(self):
-        """Test ints <= 0 for y"""
-        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-            s = Square(1, 1, -1)
-
-    def test_area(self):
-        """test area"""
-        self.assertEqual(self.s1.area(), 1)
-        self.assertEqual(self.s2.area(), 4)
-
-    def test_area_args(self):
-        """Test too many args for area()"""
-        with self.assertRaises(TypeError):
-            a = self.s1.area(1)
-
-    def test_display_too_many_args(self):
-        """Test display with too many args"""
-        with self.assertRaises(TypeError):
-            self.s1.display(1)
+        s1.update(5, 5, 5, 5)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 5, 'x': 5, 'size': 5, 'y': 5}
+        self.assertEqual(s1_dictionary, expected)
