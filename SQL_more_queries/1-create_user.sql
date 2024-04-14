468b1935ffa5cb1b6e498b72e7f7f63542fdbb33
rodwol <r.goniche@alustudent.com>
2024-04-14 20:35:27 +0000

    commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
--- creates the MySQL server user user_0d_1
-
-CREATE USER IF NOT EXISTS
-user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON
-*.* TO user_0d_1@localhost;
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
