89304e11e3c08be5699a71cab56873ed14fd9858
rodwol <r.goniche@alustudent.com>
2024-06-07 12:30:30 -0700

    commit

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require('request');
+/*const request = require('request');
 
 // Get the API URL from command-line arguments
 const url = process.argv[2];
@@ -25,4 +25,27 @@ request.get(url, function (error, response, body) {
     });
     console.log(count);
   }
+}); */
+
+const request = require('request');
+const starWarsUri = process.argv[2];
+let times = 0;
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body).results;
+
+  for (let i = 0; i < body.length; ++i) {
+    const characters = body[i].characters;
+
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')[5];
+
+      if (characterId === '18') {
+        times += 1;
+      }
+    }
+  }
+
+  console.log(times);
 });
